9df09fdb06d1f981770cfd858fea6fe66cab45bc
Livii Iabanzhi <iabanzhi.livii@gmail.com>
2021-07-28 10:00:02 +0900

    [fix]: fixed requirements of qt5 and qml

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
 sudo apt-get update -y
-sudo apt-get install -y qt5-qmake
+sudo apt-get install -y qt5-qmake qtdeclarative5-dev libqt5gui5
 sudo apt-get install libqt5multimedia5-plugins qml-module-qtmultimedia
 sudo apt -y install qml-module-qtquick-controls2
